3dff0298a31afe90a319b068a23f8b48def181c3
officialjaiv <bhanushalijaiv@gmail.com>
2022-01-25 14:41:15 +0530

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+//commonJS , Every file is module (by default)
+//Modules - Encapsulated Code (only share minimum)
+
+const names = require('./4-name');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavour');
+require('./7-mind-grenade')
+// console.log(data);
+
+// sayHi('cena')
+// sayHi(names.john)
+// sayHi(names.peter)
---
+++ intro.js
@@ -0,0 +1,16 @@
+const amount = 8
+
+if(amount < 10){
+    console.log('small number');
+}
+else{
+    console.log('large number');
+}
+
+console.log(__dirname)
+//console print after every 1 sec
+setInterval(() => {
+    console.log('Hello World')
+}, 1000)
+
+console.log('my first node app !!!');
